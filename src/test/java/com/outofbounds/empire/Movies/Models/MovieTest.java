9004872ddb55c747340d469b3026b74f7d24e278
Jonathan Agerfeld Lenz <jona8772@stud.kea.dk>
2017-09-29 10:44:24 +0200

    til anders

---
+++ src/test/java/com/outofbounds/empire/Movies/Models/MovieTest.java
@@ -0,0 +1,76 @@
+package com.outofbounds.empire.Movies.Models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MovieTest {
+    @Test
+    public void getShowings() throws Exception {
+    }
+
+    @Test
+    public void setShowings() throws Exception {
+    }
+
+    @Test
+    public void getId() throws Exception {
+    }
+
+    @Test
+    public void setId() throws Exception {
+    }
+
+    @Test
+    public void getTitle() throws Exception {
+    }
+
+    @Test
+    public void setTitle() throws Exception {
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+    }
+
+    @Test
+    public void setDescription() throws Exception {
+    }
+
+    @Test
+    public void getAgeLimit() throws Exception {
+    }
+
+    @Test
+    public void setAgeLimit() throws Exception {
+    }
+
+    @Test
+    public void getCategory() throws Exception {
+    }
+
+    @Test
+    public void setCategory() throws Exception {
+    }
+
+    @Test
+    public void getPrice() throws Exception
+    {
+    }
+
+    @Test
+    public void setPrice() throws Exception
+    {
+    }
+
+    @Test
+    public void getImageUrl() throws Exception
+    {
+    }
+
+    @Test
+    public void setImageUrl() throws Exception
+    {
+    }
+
+}
